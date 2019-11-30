a1809d11331b19a0041eefd6cb148b88a6c961d4
Axel ESSONO <naxelcdric@gmail>
2019-11-30 05:04:04 +0100

    Design login Activity UI
    Make the code to go to LoginActivity ui from the mainactivity

--- app/src/main/java/com/axel/akcosmetics/LoginActivity.java
+++ app/src/main/java/com/axel/akcosmetics/LoginActivity.java
@@ -3,11 +3,14 @@ package com.axel.akcosmetics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
--- app/src/main/java/com/axel/akcosmetics/MainActivity.java
+++ app/src/main/java/com/axel/akcosmetics/MainActivity.java
@@ -2,7 +2,9 @@ package com.axel.akcosmetics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
@@ -18,5 +20,16 @@ public class MainActivity extends AppCompatActivity
 
         joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
         longinButton = (Button) findViewById(R.id.main_login_btn);
+
+        //On click on login button
+        longinButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
