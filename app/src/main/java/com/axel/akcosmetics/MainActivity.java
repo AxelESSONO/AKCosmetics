965c9d2e0a8fa475a01ff5200a750beb1ddf648c
Axel ESSONO <naxelcdric@gmail>
2019-11-30 03:08:52 +0100

    Added login button and join button.

--- app/src/main/java/com/axel/akcosmetics/MainActivity.java
+++ app/src/main/java/com/axel/akcosmetics/MainActivity.java
@@ -3,12 +3,20 @@ package com.axel.akcosmetics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+
+    private Button joinNowButton, longinButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
+        longinButton = (Button) findViewById(R.id.main_login_btn);
     }
 }
