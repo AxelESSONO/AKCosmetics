f2a381298a0e8b68ab303afd0e6b0d5fe46e2796
Axel ESSONO <naxelcdric@gmail>
2019-12-01 05:37:19 +0100

    get Category Name

--- app/src/main/java/com/axel/akcosmetics/LoginActivity.java
+++ app/src/main/java/com/axel/akcosmetics/LoginActivity.java
@@ -141,7 +141,7 @@ public class LoginActivity extends AppCompatActivity
                                 Toast.makeText(LoginActivity.this, "Vous êtes désormais connecté", Toast.LENGTH_SHORT).show();
                                 loadingBar.dismiss();
 
-                                Intent intent = new Intent(LoginActivity.this, AdminAddNewProductActivity.class);
+                                Intent intent = new Intent(LoginActivity.this, AdminCategoryActivity.class);
                                 startActivity(intent);
 
                             }
