258381c51e8f6a7a1f1d1ca27dd9862fea60d7d7
Axel ESSONO <naxelcdric@gmail>
2019-12-02 02:41:21 +0100

    Displaying Products on Home Activity using Firebase RecyclerView

---
+++ app/src/main/java/com/axel/akcosmetics/ViewHold/ProductViewHolder.java
@@ -0,0 +1,40 @@
+package com.axel.akcosmetics.ViewHold;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.axel.akcosmetics.Interface.ItemClickListner;
+import com.axel.akcosmetics.R;
+
+public class ProductViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+{
+
+    public TextView txtProductName, txtProductDescription, txtProductPrice;
+    public ImageView imageView;
+    ItemClickListner listner;
+
+    public ProductViewHolder(@NonNull View itemView)
+    {
+        super(itemView);
+
+        imageView = (ImageView) itemView.findViewById(R.id.product_image);
+        txtProductName = (TextView) itemView.findViewById(R.id.product_name);
+        txtProductDescription = (TextView) itemView.findViewById(R.id.product_description);
+        txtProductPrice = (TextView) itemView.findViewById(R.id.product_price);
+    }
+
+    public void setItemClickListner(ItemClickListner listner)
+    {
+        this.listner = listner;
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        listner.onClick(v, getAdapterPosition(), false);
+    }
+}
