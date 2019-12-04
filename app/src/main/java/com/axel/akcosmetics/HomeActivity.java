96ba7d9effa7db15bee6b7ddf4f037c25bac2c7c
Axel ESSONO <naxelcdric@gmail>
2019-12-04 01:24:20 +0100

    Display user items on Cart List

--- app/src/main/java/com/axel/akcosmetics/HomeActivity.java
+++ app/src/main/java/com/axel/akcosmetics/HomeActivity.java
@@ -58,14 +58,15 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         toolbar.setTitle("Home");
         setSupportActionBar(toolbar);
 
-
+        /// Floating Button//////////////////////////////////////////////
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener()
         {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(HomeActivity.this, CartActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -201,6 +202,9 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
         if (id == R.id.nav_cart)
         {
+
+            Intent intent = new Intent(HomeActivity.this, CartActivity.class);
+            startActivity(intent);
 
         }
         else if (id == R.id.nav_orders)
